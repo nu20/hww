09edb23168316bf6267bb80499a79f172ec3febf
nu20 <bibhutipatra123@gmail.com>
2024-11-17 18:56:24 +0530

    first commit

---
+++ hw.py
@@ -0,0 +1,6 @@
+SetA = {1 , 2 , 4 , 5 ,6}
+SetB = {2 , 3 , 7 , 8 , 1}
+symmetric_differenceSetAandSetB = SetA.symmetric_difference(SetB)
+print ("Set B : ", (SetA) )
+print ("Set A : ", (SetB) ) 
+print ("A symmetric_difference B"  : symmetric_differenceAandB )
